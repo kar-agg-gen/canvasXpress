62f4c9f54c3e08a7a242d1dfd21c6dab4923260e
kar-agg-gen <kartikeya@aggregate-genius.com>
2019-06-25 15:52:37 +0000

    reading from package data

--- tests/testthat/test-other-segregation-tooltips.R
+++ tests/testthat/test-other-segregation-tooltips.R
@@ -1,7 +1,7 @@
 context("canvasXpress Charts - Segregated Boxplot Tooltips")
 
-y = read.table("http://www.canvasxpress.org/data/cX-toothgrowth-dat.txt", header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
-x = read.table("http://www.canvasxpress.org/data/cX-toothgrowth-smp.txt", header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
+y = read.table(system.file("extdata", "cX-toothgrowth-dat.txt.gz", package = "canvasXpress"), header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
+x = read.table(system.file("extdata", "cX-toothgrowth-smp.txt.gz", package = "canvasXpress"), header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
 
 x$dose[x$supp == "VC"] <- rep(c(1,2,3),10)
 x$dose[x$supp == "OJ"] <- rep(c(4,5),15)
