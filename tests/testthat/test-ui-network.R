1e1fd5fa1f06d9e2c98b70c293ecacc3791a23ef
kar-agg-gen <kartikeya@aggregate-genius.com>
2019-09-20 19:34:38 +0000

    removing warning as it matches with web version

--- tests/testthat/test-ui-network.R
+++ tests/testthat/test-ui-network.R
@@ -3,8 +3,6 @@ context("canvasXpress Web Charts - Network")
 
 test_that("cXnetwork1", {
     check_ui_test(cXnetwork1())
-
-    warning('layout depends on space/etc, may be off from web example')
 })
 
 test_that("cXnetwork2", {
@@ -27,20 +25,14 @@ test_that("cXnetwork5", {
 
 test_that("cXnetwork6", {
     check_ui_test(cXnetwork6())
-
-    warning('layout depends on space/etc, may be off from web example')
 })
 
 test_that("cXnetwork7", {
     check_ui_test(cXnetwork7())
-
-    warning('legend position and appearance may depend on browser')
 })
 
 test_that("cXnetwork8", {
     check_ui_test(cXnetwork8())
-
-    warning('legend position and appearance may depend on browser')
 })
 
 test_that("cXnetwork9", {
@@ -49,8 +41,6 @@ test_that("cXnetwork9", {
 
 test_that("cXnetwork10", {
     check_ui_test(cXnetwork10())
-
-    warning('legend position and appearance may depend on browser')
 })
 
 test_that("cXnetwork11", {
