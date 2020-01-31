9b4a746485fda2cf3b7b722970eabfe5ef2fadad
kar-agg-gen <kartikeya@aggregate-genius.com>
2020-01-31 16:09:14 +0000

    warning message

--- tests/testthat/test-ui-chord.R
+++ tests/testthat/test-ui-chord.R
@@ -4,13 +4,13 @@ context("canvasXpress Web Charts - Chord")
 test_that("cXchord1", {
     check_ui_test(cXchord1())
 
-    warning("Hovering over legend  fade complete plot")
+    warning("Hovering over legend fade complete plot")
 })
 
 test_that("cXchord2", {
     check_ui_test(cXchord2())
 
-    warning("Hovering over legend  fade complete plot")
+    warning("Hovering over legend fade complete plot")
 })
 
 test_that("cXchord3", {
